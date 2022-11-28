74dd0902244fdba4cfe0a19e9d2b40d4917c307a
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-11-28 17:40:20 +0800

    create function if else in public void display

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace StackUsingSingleLinkList
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StackUsingSingleLinkList
 {
     class node
     {
@@ -37,6 +42,19 @@
         {
             Console.WriteLine("\n The poped elements is: " + top.info);
             top = top.next; // make top point to the next node squence
+        }
+        public void display()
+        {
+            node tmp;
+            if (empty())
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+            }
         }
     }
 }
