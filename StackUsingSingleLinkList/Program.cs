05d576d1f24374b42c80a04a3142222c6c62c4ee
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-11-28 17:57:33 +0800

    create case 4

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -88,6 +88,10 @@ namespace StackUsingSingleLinkList
                     case '3':
                         s.display();
                         break;
+                    case '4':
+                        return;
+                    default:
+                        Console.WriteLine("\nInvalid Choice");
 
                 }
             }
